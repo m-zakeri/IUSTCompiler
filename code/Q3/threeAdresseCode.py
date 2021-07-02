4320d1e236ea23c8f50e8f0a71dee718bd12b0f6
arisaghafari <arisa_ghafari@outlook.com>
2021-07-03 01:12:10 +0430

    Q3

--- code/Q3/threeAdresseCode.py
+++ code/Q3/threeAdresseCode.py
@@ -1,8 +1,11 @@
 import argparse
 
-from ..gen.MiniJavaLexer import MiniJavaLexer
-from ..gen.MiniJavaParser import MiniJavaParser, CommonTokenStream
-from ..gen.MiniJavaListener import MiniJavaListener, ParseTreeWalker, FileStream
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+
+from Q3.gen.MiniJavaLexer import MiniJavaLexer
+from Q3.gen.MiniJavaListener import MiniJavaListener
+from Q3.gen.MiniJavaParser import MiniJavaParser
+
 
 class ThreeAddressCode(MiniJavaListener):
     def __init__(self):
@@ -24,6 +27,23 @@ class ThreeAddressCode(MiniJavaListener):
     def remove_temp(self):
         self.temp_counter -= 1
 
+    def exitIf_statment(self, ctx : MiniJavaParser.If_statmentContext):
+        ctx.statement(1).value_attr = self.CreateLabel() + ' ' + ctx.statement(1).value_attr
+        ctx.value_attr = 'if' + '!(' + ctx.expression().value_attr + ') ' + 'goto ' + ctx.statement(1).value_attr[0: 2]
+
+        print(ctx.value_attr, ctx.statement(1).value_attr)
+
+    def enterWhile_statment(self, ctx:MiniJavaParser.While_statmentContext):
+        ctx.value_attr = self.CreateLabel() + ' if' + '!(' + ctx.expression().value_attr + ')' + 'goto' + ctx.statement().value_attr[0 : 2]
+        ctx.statement().value_attr = ctx.statement().value_attr + 'goto'
+        ctx.statement().value_attr = self.CreateLabel() + ctx.statement().value_attr
+
+        print(ctx.value_attr)
+
+    def exitEqual_statmentContext(self, ctx: MiniJavaParser.Equal_statmentContext):
+        ctx.value_attr = ctx.identifier().getText() + ' = ' + ctx.expression().value_attr
+
+        print(ctx.value_attr)
 
 def main(args):
     stream = FileStream(args.file, encoding='utf8')
