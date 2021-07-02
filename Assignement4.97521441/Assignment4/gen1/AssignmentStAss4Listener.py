c0e88de0871d114166450b918541a12fa9407dec
Fgholizade <gholizadehfateme37@gmail.com>
2021-07-02 23:48:39 +0430

    HW41441

---
+++ Assignement4.97521441/Assignment4/gen1/AssignmentStAss4Listener.py
@@ -0,0 +1,339 @@
+# Generated from C:/Users/1379fgh/Documents/Compiler/Assignment4\AssignmentStAss4.g4 by ANTLR 4.9.1
+from antlr4 import *
+
+# This class defines a complete listener for a parse tree produced by AssignmentStAss4Parser.
+class AssignmentStAss4Listener(ParseTreeListener):
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#program.
+    def enterProgram(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#program.
+    def exitProgram(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#mainClass.
+    def enterMainClass(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#mainClass.
+    def exitMainClass(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#mainClassDeclare.
+    def enterMainClassDeclare(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#mainClassDeclare.
+    def exitMainClassDeclare(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#mainMethodDecl.
+    def enterMainMethodDecl(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#mainMethodDecl.
+    def exitMainMethodDecl(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#mainMethodBody.
+    def enterMainMethodBody(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#mainMethodBody.
+    def exitMainMethodBody(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#classDeclare.
+    def enterClassDeclare(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#classDeclare.
+    def exitClassDeclare(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#varDeclation.
+    def enterVarDeclation(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#varDeclation.
+    def exitVarDeclation(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#methodDecl.
+    def enterMethodDecl(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#methodDecl.
+    def exitMethodDecl(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#methodBody.
+    def enterMethodBody(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#methodBody.
+    def exitMethodBody(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#type.
+    def enterType(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#type.
+    def exitType(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_if.
+    def enterStatement_if(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_if.
+    def exitStatement_if(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_lb.
+    def enterStatement_lb(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_lb.
+    def exitStatement_lb(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_while.
+    def enterStatement_while(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_while.
+    def exitStatement_while(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_system.
+    def enterStatement_system(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_system.
+    def exitStatement_system(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_equalwithbra.
+    def enterStatement_equalwithbra(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_equalwithbra.
+    def exitStatement_equalwithbra(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#statement_equal.
+    def enterStatement_equal(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#statement_equal.
+    def exitStatement_equal(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#ifst.
+    def enterIfst(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#ifst.
+    def exitIfst(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_pran.
+    def enterExp_pran(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_pran.
+    def exitExp_pran(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_and.
+    def enterExp_and(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_and.
+    def exitExp_and(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_exclamation.
+    def enterExp_exclamation(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_exclamation.
+    def exitExp_exclamation(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_tru.
+    def enterExp_tru(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_tru.
+    def exitExp_tru(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_gt.
+    def enterExp_gt(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_gt.
+    def exitExp_gt(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_exp.
+    def enterExp_exp(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_exp.
+    def exitExp_exp(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_multi.
+    def enterExp_multi(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_multi.
+    def exitExp_multi(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_in.
+    def enterExp_in(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_in.
+    def exitExp_in(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_false.
+    def enterExp_false(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_false.
+    def exitExp_false(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_di.
+    def enterExp_di(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_di.
+    def exitExp_di(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_lb.
+    def enterExp_lb(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_lb.
+    def exitExp_lb(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_this.
+    def enterExp_this(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_this.
+    def exitExp_this(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_id.
+    def enterExp_id(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_id.
+    def exitExp_id(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_Dot.
+    def enterExp_Dot(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_Dot.
+    def exitExp_Dot(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_binaryop.
+    def enterExp_binaryop(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_binaryop.
+    def exitExp_binaryop(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_ide.
+    def enterExp_ide(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_ide.
+    def exitExp_ide(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_minus.
+    def enterExp_minus(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_minus.
+    def exitExp_minus(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#exp_sum.
+    def enterExp_sum(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#exp_sum.
+    def exitExp_sum(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#identifier.
+    def enterIdentifier(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#identifier.
+    def exitIdentifier(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by AssignmentStAss4Parser#binOperator.
+    def enterBinOperator(self, ctx):
+        pass
+
+    # Exit a parse tree produced by AssignmentStAss4Parser#binOperator.
+    def exitBinOperator(self, ctx):
+        pass
+
+
---
+++ Assignement4.97521441/Assignment4/gen1/ThreecodeListener.py
@@ -0,0 +1,186 @@
+# Generated from C:/Users/1379fgh/Documents/Compiler/Assignment4\Threecode.g4 by ANTLR 4.9.1
+from antlr4 import *
+
+# This class defines a complete listener for a parse tree produced by ThreecodeParser.
+class ThreecodeListener(ParseTreeListener):
+
+    # Enter a parse tree produced by ThreecodeParser#program.
+    def enterProgram(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#program.
+    def exitProgram(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#mainClass.
+    def enterMainClass(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#mainClass.
+    def exitMainClass(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#mainClassDeclare.
+    def enterMainClassDeclare(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#mainClassDeclare.
+    def exitMainClassDeclare(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#mainMethodDecl.
+    def enterMainMethodDecl(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#mainMethodDecl.
+    def exitMainMethodDecl(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#mainMethodBody.
+    def enterMainMethodBody(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#mainMethodBody.
+    def exitMainMethodBody(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#classDeclare.
+    def enterClassDeclare(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#classDeclare.
+    def exitClassDeclare(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#varDeclation.
+    def enterVarDeclation(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#varDeclation.
+    def exitVarDeclation(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#methodDecl.
+    def enterMethodDecl(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#methodDecl.
+    def exitMethodDecl(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#methodBody.
+    def enterMethodBody(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#methodBody.
+    def exitMethodBody(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#type.
+    def enterType(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#type.
+    def exitType(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_if.
+    def enterStatement_if(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_if.
+    def exitStatement_if(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_lb.
+    def enterStatement_lb(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_lb.
+    def exitStatement_lb(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_while.
+    def enterStatement_while(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_while.
+    def exitStatement_while(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_system.
+    def enterStatement_system(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_system.
+    def exitStatement_system(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_equalwithbra.
+    def enterStatement_equalwithbra(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_equalwithbra.
+    def exitStatement_equalwithbra(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#statement_equal.
+    def enterStatement_equal(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#statement_equal.
+    def exitStatement_equal(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#ifst.
+    def enterIfst(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#ifst.
+    def exitIfst(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#exp.
+    def enterExp(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#exp.
+    def exitExp(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#identifier.
+    def enterIdentifier(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#identifier.
+    def exitIdentifier(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ThreecodeParser#binOperator.
+    def enterBinOperator(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ThreecodeParser#binOperator.
+    def exitBinOperator(self, ctx):
+        pass
+
+
