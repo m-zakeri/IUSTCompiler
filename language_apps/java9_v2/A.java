34efaf4be019128d5b6f0a626b028c58f7f99393
m-zakeri <m-zakeri@live.com>
2022-02-12 22:55:04 +0330

    Update example source codes

--- language_apps/java9_v2/A.java
+++ language_apps/java9_v2/A.java
@@ -1,9 +1,34 @@
-/* Before refactoring (Original version) */
+/*
+Class A:
+Before refactoring (Original version) */
 class A
 {
     public int f; /* public field */
-    void m(int i)
+    private double x = 10.5;
+    private double x2 = 10.5;
+    public void m(int i)
     {
         this.f = i * this.f;
     }
+}
+
+// class B
+class B {
+public A a = new A();
+
+}
+
+/* class C perform task t*/
+class C {
+protected A a = new A();
+private int x = 10;
+
+
+}
+
+
+class Z
+
+{
+
 }
